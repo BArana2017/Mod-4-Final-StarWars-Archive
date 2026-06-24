c08476010bfb13833913e672ddb73ec012eb0d9c
Bianca <barana99@yahoo.com>
2026-06-24 07:14:48 -0500

    input

--- script.js
+++ script.js
@@ -21,8 +21,14 @@ function updateSlider() {
   maxValue.textContent = max;
 
   // calculate %
-  const minPercent = (min / minRange.max) * 100;
-  const maxPercent = (max / maxRange.max) * 100;
+const minLimit = parseInt(minRange.min);
+const maxLimit = parseInt(minRange.max);
+
+const minPercent = ((min - minLimit) / (maxLimit - minLimit)) * 100;
+const maxPercent = ((max - minLimit) / (maxLimit - minLimit)) * 100;
+
+progress.style.left = minPercent + "%";
+progress.style.width = (maxPercent - minPercent) + "%";
 
   // move progress bar
   progress.style.left = minPercent + "%";
@@ -33,3 +39,25 @@ minRange.addEventListener("input", updateSlider);
 maxRange.addEventListener("input", updateSlider);
 
 updateSlider();
+
+// Characters
+
+const searchBar = document.getElementById("search-bar");
+const results = document.getElementById("results");
+
+let characters = [];
+
+fetch("https://akabab.github.io/starwars-api/api/all.json")
+  .then((res) => res.json())
+  .then((data) => {
+    characters = data;
+  });
+
+searchBar.addEventListener("input", (e) => {
+  const search = e.target.value.toLowerCase();
+
+  results.innerHTML = characters
+    .filter((character) => character.name.toLowerCase().includes(search))
+    .map((character) => `<p>${character.name}</p>`)
+    .join("");
+});
